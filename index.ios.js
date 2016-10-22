91a5dd0e3cb1ba9ce71d3e7e369199bd7e49fa4d
James Spence <Jamesaspence20@gmail.com>
2016-10-22 11:01:26 -0500

    cleaned up initial view, made android match

--- index.ios.js
+++ index.ios.js
@@ -13,33 +13,8 @@ import WelcomeScene from './scenes/WelcomeScene';
 
 export default class umbrellaReactNative extends Component {
     render() {
-        let initialRoute = {
-            title: 'Welcome',
-            index: 0
-        };
-        return (
-            <WelcomeScene/>
-        );
+        return <WelcomeScene/>;
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
 
 AppRegistry.registerComponent('umbrellaReactNative', () => umbrellaReactNative);
