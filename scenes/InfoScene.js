bb46b7af70936c4f8bece859b40f3fb6b899ff48
James Spence <Jamesaspence20@gmail.com>
2016-10-25 19:50:13 -0500

    separated slides into their own component

--- scenes/InfoScene.js
+++ scenes/InfoScene.js
@@ -11,24 +11,13 @@ import {
     Image
 } from 'react-native';
 import GlobalStyles from './../resources/styles/global';
-import Button from './components/Button';
+import InfoScreen from './components/InfoScreen';
 
 const styles = StyleSheet.create({
     flex: {
         flex: 1,
         alignItems: 'center'
-    },
-    flexOne: {
-        flex: 1
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        alignItems: 'flex-end'
-    },
-    button: {
-        marginBottom: 90,
-        flex: 6,
-    },
+    }
 });
 
 export default class InfoScene extends React.Component {
@@ -52,37 +41,22 @@ export default class InfoScene extends React.Component {
     render() {
         return (
             <Swiper showsPagination={true} loop={false} ref={this.onSwiperMount} style={[GlobalStyles.layout.root]}>
-                <View style={styles.flex}>
-                    <Text style={[GlobalStyles.text.hero]}>
-                        Umbrella is here to help you reduce your anxiety, feel better about yourself, and have a brighter day.
-                    </Text>
-                    <View style={[styles.flexOne, styles.buttonContainer]}>
-                        <View style={styles.flexOne}/>
-                        <Button style={styles.button} text="Cool" onPress={this.goForward}/>
-                        <View style={styles.flexOne}/>
-                    </View>
-                </View>
-                <View style={styles.flex}>
-                    <Text style={[GlobalStyles.text.hero]}>
-                        Use us in the moment when you're having a bad day.
-                    </Text>
-                    <Text style={GlobalStyles.text.subHero}>
-                        We'll help you relax and calm down when you're struggling.
-                    </Text>
-                    <View style={[styles.flexOne, styles.buttonContainer]}>
-                        <View style={styles.flexOne}/>
-                        <Button style={styles.button} text="Great" onPress={this.goForward}/>
-                        <View style={styles.flexOne}/>
-                    </View>
-                </View>
-                <View style={styles.flex}>
-                    <Text style={[GlobalStyles.text.hero]}>
-                        Use us every day to build a brighter future.
-                    </Text>
-                    <Text style={GlobalStyles.text.subHero}>
-                        Check in with us to view long-term goals, progress, and work towards a happier you.
-                    </Text>
-                </View>
+                <InfoScreen style={styles.flex}
+                    hero="Umbrella is here to help you reduce your anxiety, feel better about yourself, and have a brighter day."
+                    onPress={this.goForward}
+                    buttonText="Cool"
+                    />
+                <InfoScreen style={styles.flex}
+                    hero="Use us in the moment when you're having a bad day."
+                    subHero="We'll help you relax and calm down when you're struggling."
+                    onPress={this.goForward}
+                    buttonText="Great"
+                    />
+                <InfoScreen style={styles.flex}
+                    hero="Use us every day to build a brighter future."
+                    subHero="Check in with us to view long-term goals, progress, and work towards a happier you."
+                    buttonText="Show Me How"
+                    />
             </Swiper>
         );
     }
