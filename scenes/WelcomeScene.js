547858dd6c9de602453b83a43c79b5c5797c40d1
Theo Syslack <theosyslack@gmail.com>
2016-10-26 21:08:55 -0500

    jscs fixes

--- scenes/WelcomeScene.js
+++ scenes/WelcomeScene.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 // @flow
 
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {
     View,
     Text,
@@ -11,34 +11,34 @@ import {
     StyleSheet,
     TouchableOpacity,
     Image
-} from 'react-native';
+} from "react-native";
 
-import GlobalStyles from './../resources/styles/global';
+import GlobalStyles from "./../resources/styles/global";
 
-import Button from './components/Button';
+import Button from "./components/Button";
 
-const logo = require('../resources/img/logo.png');
+const logo = require("../resources/img/logo.png" );
 
 const styles = StyleSheet.create({
     logo: {
         margin: 20,
         width: null,
         height: null,
-        resizeMode: 'contain'
+        resizeMode: "contain"
     },
     rootView: {
-        alignItems: 'stretch'
+        alignItems: "stretch"
     },
     alignCenter: {
-        alignItems: 'center'
+        alignItems: "center"
     },
     buttonContainer: {
-        flexDirection: 'row',
-        alignItems: 'flex-end'
+        flexDirection: "row",
+        alignItems: "flex-end"
     },
     button: {
         marginBottom: 50,
-        flex: 6,
+        flex: 6
     },
     flexOne: {
         flex: 1
@@ -47,24 +47,24 @@ const styles = StyleSheet.create({
 
 const animatedProperties = {
     textView: {
-        translateY: new Animated.Value(100),
-        opacity: new Animated.Value(0),
+        translateY: new Animated.Value( 100 ),
+        opacity: new Animated.Value( 0 )
     },
     logo: {
-        translateY: new Animated.Value(1000)
+        translateY: new Animated.Value( 1000 )
     },
     letsGoButton: {
-        opacity: new Animated.Value(0)
+        opacity: new Animated.Value( 0 )
     }
 };
 
 const animationStyles = {
     logo: {
-        transform: [{translateY: animatedProperties.logo.translateY}]
+        transform: [ { translateY: animatedProperties.logo.translateY } ]
     },
     heroAndSubHero: {
         opacity: animatedProperties.textView.opacity,
-        transform: [{translateY: animatedProperties.textView.translateY}]
+        transform: [ { translateY: animatedProperties.textView.translateY } ]
     },
     letsGoButton: {
         opacity: animatedProperties.letsGoButton.opacity
@@ -74,65 +74,62 @@ const animationStyles = {
 export default class WelcomeScene extends Component {
 
     constructor(props) {
-        super(props);
+        super( props );
         this.state = {
             animatedProperties,
             animationStyles
         };
 
-        this.animateLogoIn = this.animateLogoIn.bind(this);
-        this.animateHeroAndSubheroIn = this.animateHeroAndSubheroIn.bind(this);
-        this.animatedLetsGoButtonIn = this.animatedLetsGoButtonIn.bind(this);
-        this.onLoadAnimation = this.onLoadAnimation.bind(this)
+        this.animateLogoIn = this.animateLogoIn.bind( this );
+        this.animateHeroAndSubheroIn = this.animateHeroAndSubheroIn.bind( this );
+        this.animatedLetsGoButtonIn = this.animatedLetsGoButtonIn.bind( this );
+        this.onLoadAnimation = this.onLoadAnimation.bind( this );
     }
 
 
     // React Component Lifecycle
+
     componentDidMount() {
         this.onLoadAnimation().start();
     }
 
-    //Animations
+    // Animations
+
     animateLogoIn() {
-        return Animated.spring(this.state.animatedProperties.logo.translateY, {
-            toValue: 0,
-            duration: 200
-        });
+        return Animated.spring( this.state.animatedProperties.logo.translateY, { toValue: 0 });
     }
 
     animateHeroAndSubheroIn() {
-        return Animated.parallel([
-            Animated.spring(this.state.animatedProperties.textView.translateY, {toValue: 0, duration: 200}),
-            Animated.spring(this.state.animatedProperties.textView.opacity, {toValue: 1, duration: 200})
-        ])
+        return Animated.stagger( 200, [
+            Animated.spring( this.state.animatedProperties.textView.translateY, { toValue: 0 }),
+            Animated.spring( this.state.animatedProperties.textView.opacity, { toValue: 1 })
+        ]);
     }
 
     animatedLetsGoButtonIn() {
-        return Animated.spring(this.state.animatedProperties.letsGoButton.opacity, {
-            toValue: 1,
-            duration: 200
-        });
+        return Animated.spring( this.state.animatedProperties.letsGoButton.opacity, { toValue: 1 });
     }
 
 
     onLoadAnimation() {
-        return Animated.sequence([ // One after the other
-            Animated.delay(400),
-            this.animateLogoIn(),
-            Animated.delay(200),
-            this.animateHeroAndSubheroIn(),
+        return Animated.sequence([
+            Animated.stagger( 100, [
+                this.animateLogoIn(),
+                this.animateHeroAndSubheroIn()
+            ]),
+            Animated.delay( 1000 ),
             this.animatedLetsGoButtonIn()
         ]);
     }
 
     render() {
         return (
-            <View style={[styles.rootView, styles.flexOne, GlobalStyles.layout.root]}>
+            <View style={[ styles.rootView, styles.flexOne, GlobalStyles.layout.root ]}>
                 <Animated.Image className="logo" source={logo}
-                                style={[styles.logo, styles.flexOne, this.state.animationStyles.logo]}/>
-                <Animated.View style={[styles.alignCenter, styles.flexOne, this.state.animationStyles.heroAndSubHero]}>
-                    <View className="textView" style={[styles.alignCenter]}>
-                        <Text style={[GlobalStyles.text.hero]}>
+                                style={[ styles.logo, styles.flexOne, this.state.animationStyles.logo ]}/>
+                <Animated.View style={[ styles.alignCenter, styles.flexOne, this.state.animationStyles.heroAndSubHero ]}>
+                    <View className="textView" style={[ styles.alignCenter ]}>
+                        <Text style={[ GlobalStyles.text.hero ]}>
                             Umbrella
                         </Text>
                         <Text style={GlobalStyles.text.subHero}>
@@ -140,12 +137,12 @@ export default class WelcomeScene extends Component {
                         </Text>
                     </View>
                     <Animated.View
-                        style={[styles.flexOne, styles.buttonContainer, this.state.animationStyles.letsGoButton]}>
+                        style={[ styles.flexOne, styles.buttonContainer, this.state.animationStyles.letsGoButton ]}>
                         <View style={styles.flexOne}/>
                         <Button
                             text="Let's Go"
                             style={styles.button}
-                            onPress={this.props.onContinue.bind(this)}
+                            onPress={this.props.onContinue.bind( this )}
                         />
                         <View style={styles.flexOne}/>
                     </Animated.View>
