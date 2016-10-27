4a19a1296fc178fbdf0a3cdbfb0982cc9723750d
Theo Syslack <theosyslack@gmail.com>
2016-10-27 16:07:33 -0500

    Renamed the app the right way!

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {
     AppRegistry,
     StyleSheet,
     Text,
     View,
     Navigator
-} from 'react-native';
+} from "react-native";
 
-import IntroFlowScene from './scenes/IntroFlowScene';
+import IntroFlowScene from "./scenes/IntroFlowScene";
 
-export default class umbrellaReactNative extends Component {
+export default class Umbrella extends Component {
     render() {
         return <IntroFlowScene/>;
     }
 }
 
-AppRegistry.registerComponent('umbrellaReactNative', () => umbrellaReactNative);
+AppRegistry.registerComponent("Umbrella", () => Umbrella );
