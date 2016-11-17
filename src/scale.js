430ae00b141ef58f483bd39ef2be924982a5bb9c
James Spence <Jamesaspence20@gmail.com>
2016-11-16 22:22:57 -0600

    scale module that handles dimension measurements

---
+++ src/scale.js
@@ -0,0 +1,19 @@
+"use strict";
+// @flow
+
+import {
+    Dimensions
+} from "react-native";
+
+const WINDOW_WIDTH = Dimensions.get("window").width;
+
+function getWidth(percentage) {
+    return WINDOW_WIDTH / (percentage / 100);
+}
+
+export default {
+    WINDOW_WIDTH: WINDOW_WIDTH,
+    WIDTH_80_PERCENT: getWidth(80),
+    WIDTH_50_PERCENT: getWidth(50),
+    getWidth: getWidth
+};
