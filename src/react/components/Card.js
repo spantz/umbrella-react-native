a81b8782f788bc1e48fc0d92fbaa7b78b7bf7a1f
James Spence <Jamesaspence20@gmail.com>
2016-11-16 22:23:09 -0600

    Card component, used for swipe cards

---
+++ src/react/components/Card.js
@@ -0,0 +1,36 @@
+"use strict";
+// @flow
+
+import React from "react";
+import {
+    View,
+    StyleSheet
+} from "react-native";
+import Scale from "../../scale";
+
+const CARD_MARGIN = 40;
+const CARD_WIDTH = Scale.WINDOW_WIDTH - CARD_MARGIN  * 2;
+const CARD_HEIGHT = CARD_WIDTH * 1.4;
+
+const styles = StyleSheet.create({
+    backgroundColor: "#fff",
+    margin: CARD_MARGIN,
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    shadowColor: "#555",
+    shadowOpacity: .3,
+    shadowRadius: 5,
+    borderRadius: 8,
+    padding: 5,
+    justifyContent: "center"
+});
+
+export default class Card extends React.Component {
+    render() {
+        return (
+            <View style={[styles, this.props.style]}>
+                {this.props.children}
+            </View>
+        );
+    }
+}
