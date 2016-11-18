b8e67d0ec7756bad0374635d5203910017e9ace8
Theo Syslack <theosyslack@gmail.com>
2016-11-18 16:28:44 -0600

    Sun animates in now

--- src/react/scenes/InfoScene.js
+++ src/react/scenes/InfoScene.js
@@ -8,16 +8,44 @@ import {
     Text,
     StyleSheet,
     TouchableOpacity,
+    Animated,
+    LayoutAnimation,
     Image
 } from "react-native";
 import GlobalStyles from "../../resources/styles/global";
 import InfoScreen from "../screens/InfoScreen";
 import ActivityIntroScene from "./ActivityIntroScene";
 
+const sun = require("../../resources/img/Questionaire/Sun.png");
+const hill = require("../../resources/img/Questionaire/Hill.png");
+
+const animation = {
+    sun: {
+        top: new Animated.Value(400),
+        left: new Animated.Value(-200),
+    },
+    hill: {
+        opacity: new Animated.Value(0),
+    }
+};
+
 const styles = StyleSheet.create({
     flex: {
         flex: 1,
         alignItems: "center"
+    },
+    sun: {
+        position: "absolute",
+        width: 157,
+        height: 157,
+
+    },
+    hill: {
+        position: "absolute",
+        right: 0,
+        top: 398,
+        width: 750/2,
+        height: 538/2
     }
 });
 
@@ -27,6 +55,7 @@ export default class InfoScene extends React.Component {
     constructor(props) {
         super(props);
 
+        this.animateInSun = this.animateInSun.bind(this);
         this.goForward = this.goForward.bind(this);
         this.onSwiperMount = this.onSwiperMount.bind(this);
         this.goToActivityIntro = this.goToActivityIntro.bind(this);
@@ -48,14 +77,34 @@ export default class InfoScene extends React.Component {
         });
     }
 
+    animateInSun() {
+        return Animated.sequence([
+            Animated.delay(1000),
+            Animated.parallel([
+                Animated.spring(animation.sun.top, { toValue: 298 }),
+                Animated.spring(animation.sun.left, { toValue: 106 })
+            ])
+        ]);
+    }
+
+    componentDidMount() {
+        this.animateInSun().start();
+    }
+
     render() {
+
         return (
             <Swiper showsPagination={true} loop={false} ref={this.onSwiperMount} style={[GlobalStyles.layout.root]}>
-                <InfoScreen style={styles.flex}
-                    hero="Umbrella is here to help you reduce your anxiety, feel better about yourself, and have a brighter day."
+                <View style={[styles.flex]}>
+                 <Animated.Image source={sun} style={[styles.sun, animation.sun]}/>
+                    <Animated.Image source={hill} style={[styles.hill]}/>
+                    <InfoScreen style={[styles.flex]}
+                    hero="Use us in the moment when you're having a bad day."
+                    subHero="We'll help you relax and calm down when you're struggling."
                     onPress={this.goForward}
-                    buttonText="Cool"
+                    buttonText="Great"
                     />
+                </View>
                 <InfoScreen style={styles.flex}
                     hero="Use us in the moment when you're having a bad day."
                     subHero="We'll help you relax and calm down when you're struggling."
