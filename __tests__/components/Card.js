dd39de2051976c1b06b0895228f931ccc1ca7e59
James Spence <Jamesaspence20@gmail.com>
2016-11-20 11:09:36 -0600

    additional tests on card

--- __tests__/components/Card.js
+++ __tests__/components/Card.js
@@ -1,10 +1,23 @@
 import "react-native";
 import React from "react";
 import Card from "../../src/react/components/Card";
+import {Text} from "react-native";
+import TestCase from "../../test-case";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
 it("renders correctly", function () {
-    renderer.create(<Card />);
+    expect(renderer.create(<Card />)).toBeDefined();
+});
+
+it("renders child components", function () {
+    const card = renderer.create(<Card><Text>Hello!</Text></Card>);
+    const children = TestCase.getChildren(card);
+
+    expect(children.length).toEqual(1);
+
+    const child = children[0];
+    expect(child.type).toEqual("Text");
+    expect(child.children[0]).toEqual("Hello!");
 });
--- test-case.js
+++ test-case.js
@@ -10,4 +10,12 @@ export default class TestCase {
     static getRenderedProps(component) {
         return component._component._instance.props.props;
     }
+
+    static getChildren(component) {
+        return this.getJSON(component).children;
+    }
+
+    static getJSON(component) {
+        return component.toJSON();
+    }
 }
