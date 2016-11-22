b120d15f60b869174a52b542556620b0b1e952a6
James Spence <Jamesaspence20@gmail.com>
2016-11-21 23:59:35 -0600

    WIP - half-built Activities Home Screen

--- src/react/scenes/ActivityHomeScene.js
+++ src/react/scenes/ActivityHomeScene.js
@@ -5,60 +5,115 @@ import React from "react";
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from "react-native";
 import GlobalStyles from "../../resources/styles/global";
-import Button from "../components/Button";
+import Scale from "../../scale";
+import Colors from "../../resources/styles/colors";
 
+const scaleHalf = Scale.WIDTH_50_PERCENT;
+const borderRadius = scaleHalf / 2;
 const styles = StyleSheet.create({
-    buttonContainer: {
-        flexDirection: "row",
-        alignItems: "flex-end"
+    root: {
+        backgroundColor: "#fff"
     },
-    flexOne: {
-        flex: 1
+    topSection: {
+        borderBottomRightRadius: borderRadius,
+        borderBottomLeftRadius: borderRadius,
+        width: scaleHalf,
+        height: scaleHalf,
+        alignSelf: "center",
+        transform: [
+            {
+                scaleX: 3.5
+            }
+        ],
+        alignItems: "center"
+    },
+    topSectionInView: {
+        width: Scale.WINDOW_WIDTH,
+        transform: [
+            {
+                scaleX: 1 / 3.5
+            }
+        ]
+    },
+    score: {
+        marginTop: 40,
+        color: Colors.white,
+        fontSize: 45,
+        fontWeight: "bold",
+        alignSelf: "center"
+    },
+    contentContainer: {
+        marginLeft: 20,
+        marginRight: 20
+    },
+    hero: {
+        color: Colors.black,
+        marginTop: 20
+    },
+    filterContainer: {
+        marginTop: 10,
+        flexDirection: "row"
     },
     button: {
-        marginBottom: 50,
-        flex: 6
+        borderColor: Colors.background,
+        borderRadius: 8,
+        flex: 2,
+        marginLeft: 5,
+        marginRight: 5,
+        borderWidth: 1
+    },
+    buttonText: {
+        color: Colors.background,
+        margin: 5,
+        textAlign: "center"
+    },
+    largeButton: {
+        flex: 3
+    },
+    buttonActive: {
+        backgroundColor: Colors.background,
+        borderColor: Colors.white
+    },
+    buttonTextActive: {
+        color: Colors.white
     }
 });
 
 export default class ActivityHomeScene extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onPress = this.onPress.bind(this);
-    }
-
-    onPress() {
-
-    }
-
     render() {
         return (
-            <View style={GlobalStyles.layout.root}>
-                <Text style={GlobalStyles.text.hero}>
-                    Activities
-                </Text>
-                <Text style={GlobalStyles.text.subHero}>
-                    Select an activity.
-                </Text>
-                <View>
-                    <View style={styles.buttonContainer}>
-                        <View style={styles.flexOne}/>
-                        <Button
-                            text="Breathe"
-                            onPress={this.onPress}
-                            style={styles.button}/>
-                        <View style={styles.flexOne}/>
+            <View style={[GlobalStyles.layout.root, styles.background]}>
+                <View style={[GlobalStyles.colors.screen, styles.topSection]}>
+                    <View style={[styles.topSectionInView]}>
+                        <Text style={styles.score}>
+                            240
+                        </Text>
                     </View>
-                    <View style={styles.buttonContainer}>
-                        <View style={styles.flexOne}/>
-                        <Button
-                            text="Mood Journal"
-                            onPress={this.onPress}
-                            style={styles.button}/>
-                        <View style={styles.flexOne}/>
+                </View>
+                <View style={styles.contentContainer}>
+                    <Text style={[GlobalStyles.text.hero, styles.hero]}>
+                        Keep it up!
+                    </Text>
+                    <View style={styles.filterContainer}>
+                        <TouchableOpacity style={[styles.button, styles.buttonActive]}>
+                            <Text style={[styles.buttonText, styles.buttonTextActive]}>
+                                New
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>
+                                Favorites
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button, styles.largeButton]}>
+                            <Text style={styles.buttonText}>
+                                Recommended
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
