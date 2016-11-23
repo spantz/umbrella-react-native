f6577bd4b13409b06e9fd8aa3ce530046efb7c01
James Spence <Jamesaspence20@gmail.com>
2016-11-22 22:15:24 -0600

    added support for half-width cards in card component

--- src/react/components/Card.js
+++ src/react/components/Card.js
@@ -8,16 +8,16 @@ import {
 } from "react-native";
 import Scale from "../../scale";
 
-const CARD_MARGIN = 40;
-const CARD_WIDTH = Scale.WINDOW_WIDTH - CARD_MARGIN  * 2;
-const CARD_HEIGHT = CARD_WIDTH * 1.2;
+const CARD_FULL_MARGIN = 40;
+const CARD_FULL_WIDTH = Scale.WINDOW_WIDTH - CARD_FULL_MARGIN * 2;
+const CARD_HALF_MARGIN = 20;
+const CARD_HALF_WIDTH = Scale.WIDTH_50_PERCENT - CARD_HALF_MARGIN * 2;
+
+const heightWidthRatio = 1.2;
 
 const styles = StyleSheet.create({
     card: {
         backgroundColor: "#fff",
-        margin: CARD_MARGIN,
-        width: CARD_WIDTH,
-        height: CARD_HEIGHT,
         shadowColor: "#555",
         shadowOpacity: .3,
         shadowRadius: 5,
@@ -25,13 +25,43 @@ const styles = StyleSheet.create({
         padding: 5,
         justifyContent: "center",
         alignItems: "center"
+    },
+    cardFull: {
+        margin: CARD_FULL_MARGIN,
+        width: CARD_FULL_WIDTH,
+        height: CARD_FULL_WIDTH * heightWidthRatio
+    },
+    cardHalf: {
+        margin: CARD_HALF_MARGIN,
+        width: CARD_HALF_WIDTH,
+        height: CARD_HALF_WIDTH * heightWidthRatio
     }
 });
 
+
+const CARD_SIZE_FULL = "full_size";
+const CARD_SIZE_HALF = "half_size";
+
 export default class Card extends React.Component {
+    static CARD_SIZE_FULL = CARD_SIZE_FULL;
+    static CARD_SIZE_HALF = CARD_SIZE_HALF;
+
+    static propTypes = {
+        size: React.PropTypes.oneOf([CARD_SIZE_FULL, CARD_SIZE_HALF])
+    };
+
+    static defaultProps = {
+        size: CARD_SIZE_FULL
+    };
+
     render() {
+        let sizeStyle = styles.cardFull;
+        if (this.props.size === this.constructor.CARD_SIZE_HALF) {
+            sizeStyle = styles.cardHalf;
+        }
+
         return (
-            <View style={[styles.card, this.props.style]}>
+            <View style={[styles.card, sizeStyle, this.props.style]}>
                 {this.props.children}
             </View>
         );
