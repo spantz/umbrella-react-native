b0ed9ae8adf198b45545fdca161764ec5205d68e
Theo Syslack <theosyslack@gmail.com>
2016-11-23 23:19:52 -0600

    Added animation factory to quickly make animations

---
+++ src/resources/styles/animations.js
@@ -0,0 +1,36 @@
+import {
+    StyleSheet,
+    Animated
+} from "react-native";
+
+
+class AnimationFactory {
+    constructor(properties) {
+        this.keys = Object.keys(properties);
+        this.styles = {};
+        this.triggers = [];
+
+        for (let i = 0, j =  this.keys.length; i < j; i++) {
+            const key = this.keys[i];
+            const start = properties[key][0] || 0;
+            const end = properties[key][1] || 0;
+
+            Object.assign(this.styles, this.createStartStyleForProperty(key, start));
+            this.triggers.push(this.createAnimationTrigger(key, end));
+        };
+
+        this.all = Animated.parallel(this.triggers);
+    }
+
+    createAnimationTrigger(key, value) {
+        return Animated.spring(this.styles[key], { toValue: value });
+    }
+
+    createStartStyleForProperty(key, value) {
+        var startStyle = {};
+        startStyle[key] = new Animated.Value(value);
+        return startStyle;
+    }
+}
+
+export default AnimationFactory;
