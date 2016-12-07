c6a74ee57289ca7044ea2a448c1247fe33853041
James Spence <Jamesaspence20@gmail.com>
2016-12-06 20:28:12 -0600

    deleted test code

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -3,14 +3,7 @@ import Card from "./Card";
 import Hero from "./Hero";
 import Subhero from "./Subhero";
 
-const test = {
-    hello: "there",
-    what: "up",
-    "stuff": "things"
-};
-
 export {
-    test,
     Button,
     Card,
     Hero,
